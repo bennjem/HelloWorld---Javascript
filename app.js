2d66f26a24eea89d0733a08155b2309c9b31ceff
Ahmed Alsubhi <bennjem@gmail.com>
2021-04-18 23:38:01 +0300

    First Work

---
+++ app.js
@@ -0,0 +1,16 @@
+curl -X POST --user "apikey:-Z0tz7ZD-7iTrqWGW14e5gkPN4VNSapAO6uGnkS_Xemx" \
+--header "Content-Type: application/json" \
+--data '{"text": ["Hello, world.", "How are you?"], "model_id":"en-ar"}' \
+"https://api.us-south.language-translator.watson.cloud.ibm.com/instances/c1389919-ef26-4eba-95ae-facbfd883ef5/v3/translate?version=2018-05-01"
+
+
+
+
+{
+  "apikey": "-Z0tz7ZD-7iTrqWGW14e5gkPN4VNSapAO6uGnkS_Xemx",
+  "iam_apikey_description": "Auto-generated for key b4cdfcf2-3f32-4d27-b27f-b26c22849f06",
+  "iam_apikey_name": "Auto-generated service credentials",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/81e92a856afc4674b1c36fdb1833b1e0::serviceid:ServiceId-35db9a18-945a-4648-91b9-23abd1c8476a",
+  "url": "https://api.us-south.language-translator.watson.cloud.ibm.com/instances/c1389919-ef26-4eba-95ae-facbfd883ef5"
+}
